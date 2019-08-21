4c52c854adf002ba902b93fbf505906878d606b4
vkradio <37143087+vkradio@users.noreply.github.com>
2019-08-21 11:43:11 +0500

    Chapters 3-4

--- QuizActivity.cs
+++ QuizActivity.cs
@@ -3,6 +3,7 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Android.Util;
 
 namespace GeoQuiz
 {
@@ -18,6 +19,9 @@ namespace GeoQuiz
             new Question(Resource.String.question_americas, true),
             new Question(Resource.String.question_asia, true)
         };
+
+        static readonly string C_LOG_TAG = nameof(QuizActivity);
+        static readonly string C_KEY_INDEX = "index";
 
         Button trueButton;
         Button falseButton;
@@ -44,8 +48,11 @@ namespace GeoQuiz
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            Log.Debug(C_LOG_TAG, "OnCreate(Bundle) called");
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_quiz);
+
+            currentIndex = savedInstanceState?.GetInt(C_KEY_INDEX, 0) ?? 0;
 
             trueButton = FindViewById<Button>(Resource.Id.TrueButton);
             falseButton = FindViewById<Button>(Resource.Id.FalseButton);
@@ -63,6 +70,44 @@ namespace GeoQuiz
                 UpdateQuestion();
             };
         }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            Log.Debug(C_LOG_TAG, "OnStart() called");
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Log.Debug(C_LOG_TAG, "OnResume() called");
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            Log.Debug(C_LOG_TAG, "OnPause() called");
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+            Log.Debug(C_LOG_TAG, "OnStop() called");
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            Log.Debug(C_LOG_TAG, "OnDestroy() called");
+        }
+
+        protected override void OnSaveInstanceState(Bundle savedInstanceState)
+        {
+            base.OnSaveInstanceState(savedInstanceState);
+            Log.Info(C_LOG_TAG, nameof(OnSaveInstanceState));
+            savedInstanceState.PutInt(C_KEY_INDEX, currentIndex);
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
